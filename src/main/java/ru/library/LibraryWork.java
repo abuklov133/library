582560b0f7a082365bf613364efb436953f522c4
abuklov133 <abuklov133@gmail.com>
2024-02-14 10:36:18 +0300

    Статические методы.

--- src/main/java/ru/library/LibraryWork.java
+++ src/main/java/ru/library/LibraryWork.java
@@ -7,63 +7,86 @@ public class LibraryWork {
         while (run) {
             int select = input.askInt("Select the action number: ");
             if (select == 0) {
-                System.out.println("The user has selected " + select);
-                System.out.println("==Add a new book to the catalog==");
-                String name = input.askStr("Enter the title of the book:  ");
-                Book book = new Book(name);
-                storage.add(book);
-                System.out.println("Added a new book: " + book);
+                createBook(input, storage);
             } else if (select == 1) {
-                System.out.println("==We display the entire list of books==");
-                Book[] books = storage.findAll();
-                if (books.length > 0) {
-                    for (Book book : books) {
-                        System.out.println(book);
-                    }
-                } else {
-                    System.out.println("There are not books in the vault yet.\n");
-                }
+                displayAllBooks(storage);
             } else if (select == 2) {
-                System.out.println("==Replacing the book in the catalog==");
-                int id = input.askInt("Enter id: ");
-                String title = input.askStr("Enter a book's title: ");
-                Book book = new Book(title);
-                if (storage.replace(id, book)) {
-                    System.out.println("The book has been successfully replaced");
-                } else {
-                    System.out.println("Replacement error");
-                }
+                replaceBook(input, storage);
             } else if (select == 3) {
-                System.out.println("==Deleting a book from a catalog==");
-                int id = input.askInt("Enter id: ");
-                Book book = storage.findById(id);
-                storage.delete(id);
-                System.out.println(book != null ? "The book was deleted successfully"
-                        : "Error of deleting");
+                deleteBook(input, storage);
             } else if (select == 4) {
-                System.out.println("==Find by number==");
-                int id = input.askInt("Enter id: ");
-                Book book = storage.findById(id);
-                if (book != null) {
-                    System.out.println("The book was found by id: " + book);
-                } else {
-                    System.out.println("The book with entered id was not found");
-                }
+                findBookByid(input, storage);
             } else if (select == 5) {
-                System.out.println("==The book will be displayed according to the entered title==");
-                String title = input.askStr("Enter a title");
-                Book[] booksByName = storage.findByName(title);
-                if (booksByName.length > 0) {
-                    for (Book book : booksByName) {
-                        System.out.println(book);
-                    }
-                } else {
-                    System.out.println("No books were found by the entered name");
-                }
+                findBooksByName(input, storage);
             } else if (select == 6) {
                 run = false;
             }
         }
+    }
+
+    public static void findBooksByName(Input input, Storage storage) {
+        System.out.println("==The book will be displayed according to the entered title==");
+        String title = input.askStr("Enter a title");
+        Book[] booksByName = storage.findByName(title);
+        if (booksByName.length > 0) {
+            for (Book book : booksByName) {
+                System.out.println(book);
+            }
+        } else {
+            System.out.println("No books were found by the entered name");
+        }
+    }
+
+    public static void findBookByid(Input input, Storage storage) {
+        System.out.println("==Find by number==");
+        int id = input.askInt("Enter id: ");
+        Book book = storage.findById(id);
+        if (book != null) {
+            System.out.println("The book was found by id: " + book);
+        } else {
+            System.out.println("The book with entered id was not found");
+        }
+    }
+
+    public static void deleteBook(Input input, Storage storage) {
+        System.out.println("==Deleting a book from a catalog==");
+        int id = input.askInt("Enter id: ");
+        Book book = storage.findById(id);
+        storage.delete(id);
+        System.out.println(book != null ? "The book was deleted successfully"
+                : "Error of deleting");
+    }
+
+    public static void replaceBook(Input input, Storage storage) {
+        System.out.println("==Replacing the book in the catalog==");
+        int id = input.askInt("Enter id: ");
+        String title = input.askStr("Enter a book's title: ");
+        Book book = new Book(title);
+        if (storage.replace(id, book)) {
+            System.out.println("The book has been successfully replaced");
+        } else {
+            System.out.println("Replacement error");
+        }
+    }
+
+    public static void displayAllBooks(Storage storage) {
+        System.out.println("==We display the entire list of books==");
+        Book[] books = storage.findAll();
+        if (books.length > 0) {
+            for (Book book : books) {
+                System.out.println(book);
+            }
+        } else {
+            System.out.println("There are not books in the vault yet.\n");
+        }
+    }
+
+    public static void createBook(Input input, Storage storage) {
+        System.out.println("==Add a new book to the catalog==");
+        String name = input.askStr("Enter the title of the book:  ");
+        Book book = new Book(name);
+        storage.add(book);
+        System.out.println("Added a new book: " + book);
     }
 
     private void showMenu() {
@@ -79,6 +102,7 @@ public class LibraryWork {
         Input input = new ConsoleInput();
         Storage storage = new Storage();
         new LibraryWork().launch(input, storage);
+        LibraryWork.createBook(input, storage);
 
     }
 }
