ba282e29e3c72f8fb25ebbbc15a3a89291416d9b
abuklov133 <abuklov133@gmail.com>
2024-02-14 09:26:59 +0300

    Разрыв зависимости LibraryWork от Scanner.

--- src/main/java/ru/library/LibraryWork.java
+++ src/main/java/ru/library/LibraryWork.java
@@ -1,26 +1,22 @@
 package ru.library;
 
-import java.util.Scanner;
-
 public class LibraryWork {
-    public void launch(Scanner scanner, Storage storage) {
+    public void launch(Input input, Storage storage) {
         showMenu();
         boolean run = true;
         while (run) {
-            System.out.print("Select the action number:");
-            int select = Integer.parseInt(scanner.nextLine());
+            int select = input.askInt("Select the action number: ");
             if (select == 0) {
                 System.out.println("The user has selected " + select);
                 System.out.println("==Add a new book to the catalog==");
-                System.out.print("Enter the title of the book:  ");
-                String name = scanner.nextLine();
+                String name = input.askStr("Enter the title of the book:  ");
                 Book book = new Book(name);
                 storage.add(book);
                 System.out.println("Added a new book: " + book);
             } else if (select == 1) {
                 System.out.println("==We display the entire list of books==");
                 Book[] books = storage.findAll();
-                if (books.length != 0) {
+                if (books.length > 0) {
                     for (Book book : books) {
                         System.out.println(book);
                     }
@@ -29,10 +25,8 @@ public class LibraryWork {
                 }
             } else if (select == 2) {
                 System.out.println("==Replacing the book in the catalog==");
-                System.out.print("Enter id: ");
-                int id = Integer.parseInt(scanner.nextLine());
-                System.out.print("Enter a book's title: ");
-                String title = scanner.nextLine();
+                int id = input.askInt("Enter id: ");
+                String title = input.askStr("Enter a book's title: ");
                 Book book = new Book(title);
                 if (storage.replace(id, book)) {
                     System.out.println("The book has been successfully replaced");
@@ -41,16 +35,14 @@ public class LibraryWork {
                 }
             } else if (select == 3) {
                 System.out.println("==Deleting a book from a catalog==");
-                System.out.print("Enter id: ");
-                int id = Integer.parseInt(scanner.nextLine());
+                int id = input.askInt("Enter id: ");
                 Book book = storage.findById(id);
                 storage.delete(id);
                 System.out.println(book != null ? "The book was deleted successfully"
                         : "Error of deleting");
             } else if (select == 4) {
                 System.out.println("==Find by number==");
-                System.out.println("Enter id: ");
-                int id = Integer.parseInt(scanner.nextLine());
+                int id = input.askInt("Enter id: ");
                 Book book = storage.findById(id);
                 if (book != null) {
                     System.out.println("The book was found by id: " + book);
@@ -59,8 +51,7 @@ public class LibraryWork {
                 }
             } else if (select == 5) {
                 System.out.println("==The book will be displayed according to the entered title==");
-                System.out.println("Enter a title");
-                String title = scanner.nextLine();
+                String title = input.askStr("Enter a title");
                 Book[] booksByName = storage.findByName(title);
                 if (booksByName.length > 0) {
                     for (Book book : booksByName) {
@@ -85,9 +76,9 @@ public class LibraryWork {
     }
 
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
+        Input input = new ConsoleInput();
         Storage storage = new Storage();
-        new LibraryWork().launch(scanner, storage);
+        new LibraryWork().launch(input, storage);
 
     }
 }
